fe98d25cd08207feddca28f6ab362f751f5c5799
Rakibul hasan Ruhan <ruhan@Rakibuls-MacBook-Air.local>
2026-08-19 01:22:34 +0600

    Update CORS with Vercel link

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173", 
+        "https://elare-frontend.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
